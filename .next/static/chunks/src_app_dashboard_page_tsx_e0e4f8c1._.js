d1d0760294d31611d72e3232a32062c292c9d369
ianikian <tikomayorista@gmail.com>
2025-07-24 18:11:24 -0300

    Add .next/static/chunks/src_app_dashboard_page_tsx_e0e4f8c1._.js 1753391483567

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_e0e4f8c1._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7f70e5f9._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_date-fns_e0d15fa9._.js",
+  "static/chunks/node_modules_react-day-picker_dist_index_esm_9fc30424.js",
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_dddff71f._.js",
+  "static/chunks/node_modules_73035ebd._.js"
+],
+    source: "dynamic"
+});
